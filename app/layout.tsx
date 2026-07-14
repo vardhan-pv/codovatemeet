ac076184357d4f1044741eabdc27a3d29c411ae0
vardhanreddy <pvvardhanreddy28@gmail.com>
2026-07-14 14:23:43 +0530

    feat: integrate Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,8 +135,27 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://images.unsplash.com" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5TV459B9');
+          `}
+        </Script>
       </head>
       <body className="font-sans antialiased bg-[#030712] text-slate-200" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TV459B9"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* JSON-LD Structured Data for Rich Google Search Results */}
         <script
           type="application/ld+json"
